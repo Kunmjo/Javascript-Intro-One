4fedca608417025c75fdcd1c1526c1bb692b7c7a
Kunmjo <jonathan.sobanjo@gmail.com>
2022-06-21 21:18:52 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const myname = "Kunmi";
+const height = "6ft";
+const country = "Nigeria";
+
+const aboutme = `My name is ${myname}, i am ${height} tall and from ${country}.`;
+
+console.log(aboutme)
